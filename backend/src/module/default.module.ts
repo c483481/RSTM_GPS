ddd025ebd749d00b68a4e8e2ffd4bebb843fcf9f
c483481 <73603369+c483481@users.noreply.github.com>
2024-11-24 00:22:01 +0800

    fix bug

--- backend/src/module/default.module.ts
+++ backend/src/module/default.module.ts
@@ -3,8 +3,7 @@ import { UserSession } from "./dto.module";
 
 export const DEFAULT_USER_SESSION_ANONYMUS: UserSession = {
     xid: "ANONYMUS",
-    email: "anonymus@anonymus.com",
-    ip: "anonymus",
+    username: "anonymus",
 };
 
 const id = {
